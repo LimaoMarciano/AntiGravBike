68520949255d4103d312b691eb9d29ce3cb8686a
Marcelo Silva <marceloapsilva@gmail.com>
2018-09-04 21:09:43 -0300

    Added Pro Builder, Pro Grids, Post Processing Stack and basic gameplay scripts

---
+++ Assets/Scripts/Antigrav.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Antigrav : MonoBehaviour {
+
+    public PIDController altitudePID;
+    public float lenght = 1.0f;
+    public float maxForce = 10.0f;
+    public float targetDistance = 5.0f;
+
+    public Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        RaycastHit hit;
+        Ray ray = new Ray(transform.position, -transform.up);
+        if (Physics.Raycast(ray, out hit, lenght))
+        {
+            float error = targetDistance - hit.distance;
+            float force = maxForce * altitudePID.Update(error);
+
+            rb.AddForceAtPosition(force * transform.up, transform.position);
+        }
+
+        Debug.DrawRay(transform.position, transform.TransformDirection (-transform.up) * lenght, Color.red);
+    }
+
+}
---
+++ Assets/Scripts/Engine.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof (Rigidbody))]
+public class Engine : MonoBehaviour {
+
+    public float maxPower = 20.0f;
+
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        float input = Input.GetAxis("Vertical");
+
+        if (input != 0)
+        {
+            rb.AddForce(maxPower * input * transform.forward);
+        }
+	}
+}
